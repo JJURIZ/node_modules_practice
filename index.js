b569bee77333893bbcd53208dc2b9429ececd289
Jeremy Uriz <jeremyuriz@Jennifers-Air.attlocal.net>
2020-11-12 17:14:36 -0500

    [stringRev, index] import, export working

---
+++ index.js
@@ -0,0 +1,22 @@
+const { stringReverser, myFavoriteFoods } = require(`./stringReverser`);
+
+let myShortString = 'a dog a plan a canal pagoda.';
+let myLongString = `I, man, am Regal, a German am I
+Never odd or even
+If I had a Hi-Fi
+Madam, I'm Adam
+Too hot to hoot
+No lemons no melon
+Too bad I hid a boot
+Lisa Bonet ate no basil
+Warsaw was raw
+Was it a car or a cat I saw?`
+
+const foodLoop = (arr) => {
+    arr.map(x => console.log(`One of my favorite foods is ${x}.`))
+}
+
+console.log(stringReverser(myShortString));
+console.log(stringReverser(myLongString));
+console.log(myFavoriteFoods);
+console.log(foodLoop(myFavoriteFoods));
---
+++ stringReverser.js
@@ -0,0 +1,11 @@
+
+const stringReverser = (str) => {
+    return str.split('').reverse().join('');
+}
+
+let myFavoriteFoods = ['chicken', 'steak', 'cheese', 'bread', 'cauliflower', 'eggs', 'sweet potatoes', 'grouper'];
+
+module.exports = {
+    stringReverser,
+    myFavoriteFoods
+}

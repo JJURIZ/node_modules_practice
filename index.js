40d62ec0b8efb2c82d2fbe45b306a0417f19facf
Jeremy Uriz <jeremyuriz@Jennifers-Air.attlocal.net>
2020-11-12 19:47:14 -0500

    [index.js] Using Lodash for first time. Cool.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+/* Imports - From project and npm packages */
 const { stringReverser, myFavoriteFoods } = require(`./stringReverser`);
 let colors = require('colors');
-let _ = require('lodash/core');
+let _ = require('lodash');
 
+/* Variables for testing imported function stringReverser */
 let myShortString = 'a dog a plan a canal pagoda.';
 let myLongString = `I, man, am Regal, a German am I
 Never odd or even
@@ -14,17 +16,37 @@ Lisa Bonet ate no basil
 Warsaw was raw
 Was it a car or a cat I saw?`
 
+console.log(stringReverser(myShortString));
+console.log(stringReverser(myLongString));
+
+/* Mapping over array of my favorite foods from the stringReverser.js file */
 const foodLoop = (arr) => {
     arr.map(x => console.log(`One of my favorite foods is ${x}.`))
 }
 
-console.log(stringReverser(myShortString));
-console.log(stringReverser(myLongString));
 console.log(myFavoriteFoods);
 console.log(foodLoop(myFavoriteFoods));
 
-
+/* Testing npm pacakage 'colors' */
 console.log('Green'.magenta);
 console.log("LISTEN TO ME!".underline.red)
-console.log(`The quick brown fox jumps over the lazy moon.`.trap);
-console.log(`The quick brown fox jumps over the lazy moon.`.rainbow);
+console.log(`The quick brown fox jumps over the lazy moon.-WITH TRAP STYLE`.trap);
+console.log(`The quick brown fox jumps over the lazy moon.- WITH RAINBOW STYLE!`.rainbow);
+
+/* Arrays for testing Lodash _chunk method */
+let testLodashTwo = _.chunk(myFavoriteFoods, [size=2]);
+let testLodashThree = _.chunk(myFavoriteFoods, [size=3]);
+let testLodashFour = _.chunk(myFavoriteFoods, [size=4]);
+
+/* Testing Lodash _chunk method */
+console.log(testLodashTwo);
+console.log(testLodashThree);
+console.log(testLodashFour);
+
+/* Falsy array for testing */
+
+let falsyArray = [1, 0 , false, true, -1, "False", [0,1,2,3,1,0]];
+let compactFalsyArray = _.compact(falsyArray);
+
+console.log(falsyArray);
+console.log(compactFalsyArray);
